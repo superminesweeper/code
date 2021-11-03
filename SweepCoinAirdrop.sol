b948a59264db1d3aefc615d1edb550cf5df0f046
superminesweeper <93648086+superminesweeper@users.noreply.github.com>
2021-11-03 17:59:46 +0800

    Add files via upload

---
+++ SweepCoinAirdrop.sol
@@ -0,0 +1,109 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
+
+import "./Address.sol";
+import './SafeMath.sol';
+import "./SweepCoin.sol";
+import "./SweepCoinEcologyPool.sol";
+
+
+contract SweepCoinAirdrop {
+    using SafeMath for uint256;
+    using Address for address;
+
+    address private _owner;
+    SweepCoin private _Token;
+    SweepCoinEcologyPool private _EcologyPool;
+
+    struct AirdropItem {
+        address _address;
+        uint256 _starttime;
+        uint256 _endtime;
+        uint256 _value;
+        uint8 _day;
+    }
+
+    //Transfer of account airdrop balance is prohibited
+    mapping(address => AirdropItem)  private _airdrop_balances;
+    uint256 public airdropTotal;
+
+    constructor (address tokenAddress, address ecologypoolAddress)  {
+        _owner = msg.sender;
+        _Token = SweepCoin(tokenAddress);
+        _EcologyPool = SweepCoinEcologyPool(ecologypoolAddress);
+    }
+
+
+    function airdropBalances(address to) public view returns (uint256) {
+        if (_airdrop_balances[to]._address == address(0)) return 0;
+        if (_airdrop_balances[to]._endtime <= block.timestamp) return 0;
+        return _airdrop_balances[to]._value;
+    }
+
+
+    function airdropInfo(address to) public view returns (AirdropItem memory) {
+        return _airdrop_balances[to];
+    }
+
+
+    function unlockAirdrop(address target) public {
+        require(address(msg.sender) == address(tx.origin), "no contract");
+        require(address(msg.sender) == _owner, "only owner  can transfer money ");
+        require(address(msg.sender) != target, "You can't transfer money to yourself");
+        require(_airdrop_balances[target]._address != address(0), "Lock record not found");
+        require(_airdrop_balances[target]._endtime > block.timestamp, "Data unlocked, do not repeat");
+        _airdrop_balances[target]._endtime = 0;
+    }
+
+
+    function sendAirdrop(address to, uint8 lockDay, uint256 value_, address master) public returns (uint256){
+        require(address(msg.sender) == address(tx.origin), "no contract");
+        require(address(msg.sender) == _owner, "only owner  can transfer money ");
+        require(address(msg.sender) != to, "You can't transfer money to yourself");
+        require(to != master, "Cannot bind from");
+        require(!to.isContract(), "no contract !!");
+        require(_owner != to, "Cannot transfer to administrator");
+        require(_Token.isSetAirdopToken() == true, 'Token airdrop information not initialized');
+
+        uint256 value = value_.mul(10 ** uint256(_Token.decimals()));
+        uint256 endtime = block.timestamp + 3600 * 24 * lockDay;
+        require(value > 0, "Amount must be greater than 0");
+        require(_airdrop_balances[to]._address == address(0), "Airdrop record already exists");
+        require(_Token.canUseBalanceOf(address(this)) >= value_, 'Insufficient contract address balance');
+
+        _airdrop_balances[to] = AirdropItem(to, block.timestamp, endtime, value, lockDay);
+        _Token.transfer(to, value);
+
+        _EcologyPool.bindApprentice(master, to);
+
+
+        airdropTotal = airdropTotal.add(value);
+
+        return value;
+    }
+
+
+    function sendAirdropBatch(address[] memory tos, uint8 lockDay, uint256 value_, address[] memory masters) public returns (uint256){
+        require(address(msg.sender) == _owner, "only owner can transfer money ");
+        require(tos.length == masters.length, "only owner can transfer money ");
+        uint256 value = value_.mul(10 ** uint256(_Token.decimals()));
+
+        uint256 total_value = 0;
+
+        for (uint8 i = 0; i < tos.length; i++) {
+            total_value = total_value.add(value);
+        }
+
+        require(_Token.canUseBalanceOf(address(this)) >= total_value, 'Insufficient contract address balance');
+
+
+        for (uint8 i = 0; i < tos.length; i++) {
+            address to = tos[i];
+            sendAirdrop(to, lockDay, value_, masters[i]);
+        }
+
+        return total_value;
+    }
+
+
+}
---
+++ SweepCoinEcologyPool.sol
@@ -0,0 +1,239 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
+
+import './SafeMath.sol';
+import "./SweepCoin.sol";
+import "./SweepNFT.sol";
+
+contract SweepCoinEcologyPool {
+    using SafeMath for uint256;
+
+    address  private _owner;
+    SweepCoin private _Token;
+    SweepNFT private _NFT;
+    address public airdropAddress;
+
+    address public miningAddress;
+
+
+    // User level direct and indirect 1000% specific revenue
+    struct LevelInfo {
+        uint8 lv;
+        uint256 direct_active_rate;
+        uint256 indirect_active_rate;
+        uint256 direct_extract_rate;
+        uint256 indirect_extract_rate;
+    }
+
+    mapping(uint8 => LevelInfo)  private levelList;
+
+    // apprentices => master
+    mapping(address => address)  private masterRelationship;
+    // Number of apprentices
+    mapping(address => address[])  private apprentices;
+    //Level of address
+    mapping(address => LevelInfo)  private addressLvs;
+
+
+    uint256 private totalWithdraw = 0;
+    // aready Withdrawn rewards
+    mapping(address => uint256)  private withdrawList;
+
+    uint256 private totalUnWithdraw = 0;
+    //Rewards to be withdrawn
+    mapping(address => uint256)  private unwithdrawList;
+    //NFT activation record
+    mapping(uint256 => uint256)  private nftActiveList;
+
+
+    constructor (address coinAddress, address nftAddress)  {
+        _owner = msg.sender;
+        _Token = SweepCoin(coinAddress);
+        _NFT = SweepNFT(nftAddress);
+
+        for (uint8 i = 1; i <= 5; i++) {
+            levelList[i] = LevelInfo(i, 0, 0, 0, 0);
+        }
+    }
+
+    // What needs to be done in the first step of releasing the contract
+    function setAirdropAddress(address target) public {
+        require(address(msg.sender) == _owner, "Insufficient permissions");
+        airdropAddress = target;
+    }
+
+
+    function setMiningAddress(address target) public {
+        require(address(msg.sender) == _owner, "Insufficient permissions");
+        miningAddress = target;
+    }
+
+
+    function updateLevelInfo(uint8 lv_,
+        uint256 direct_active_rate_, uint256 indirect_active_rate_,
+        uint256 direct_extract_rate_, uint256 indirect_extract_rate_
+    ) public {
+        require(address(msg.sender) == _owner, "Insufficient permissions");
+        require(levelList[lv_].lv > 0, "Illegal parameter");
+        levelList[lv_] = LevelInfo(lv_, direct_active_rate_, indirect_active_rate_, direct_extract_rate_, indirect_extract_rate_);
+    }
+
+
+    function levelInfo(uint8 lv_) public view returns (LevelInfo memory){
+        return levelList[lv_];
+    }
+
+
+    function setAddressLv(address target, uint8 lv_) public {
+        require(address(msg.sender) == _owner, "Insufficient permissions");
+        require(levelList[lv_].lv > 0, "Illegal parameter");
+        addressLvs[target] = levelList[lv_];
+    }
+
+
+    function getAddressLv(address target) public view returns (LevelInfo memory) {
+        LevelInfo memory info = addressLvs[target];
+
+        if (info.lv == 0) return levelList[1];
+
+        return info;
+    }
+
+
+    function getMaster(address target) public view returns (address) {
+        return masterRelationship[target];
+    }
+
+    function apprenticeNum(address target) public view returns (uint256) {
+        return apprentices[target].length;
+    }
+
+
+    function nftActiveTime(uint256 tokenId) public view returns (uint256) {
+        return nftActiveList[tokenId];
+    }
+
+
+    function bindApprentice(address master, address apprentice) public {
+        require(msg.sender == airdropAddress, 'No permission to call');
+        require(master != apprentice, 'Cannot bind from');
+
+        require(masterRelationship[apprentice] == address(0), 'Relationship already bound');
+
+        if (masterRelationship[apprentice] == master) return;
+
+
+        masterRelationship[apprentice] = master;
+        apprentices[master].push(apprentice);
+    }
+
+
+    function apprenticeAddressByIndex(address master, uint256 index_) public view returns (address) {
+        require(index_ < apprentices[master].length, 'global index out of bounds');
+        return apprentices[master][index_];
+    }
+
+
+    // Award master
+    function awardMaster(address target, uint256 amount) public {
+
+        require(miningAddress == msg.sender, 'Contract is required to call');
+        require(amount > 0, 'Withdrawal amount must be greater than 0');
+
+        address directMaster = masterRelationship[target];
+
+        if (directMaster != address(0)) {
+
+            LevelInfo memory directInfo = getAddressLv(directMaster);
+
+            uint256 directValue = directInfo.direct_active_rate.mul(amount).div(1000);
+            unwithdrawList[directMaster] = unwithdrawList[directMaster].add(directValue);
+
+            totalUnWithdraw = totalUnWithdraw.add(directValue);
+
+        }
+
+        //inDirect reward Master reward
+        if (masterRelationship[directMaster] != address(0)) {
+            address inDirectMaster = masterRelationship[directMaster];
+            LevelInfo memory indirectInfo = getAddressLv(inDirectMaster);
+
+            uint256 indirectValue = indirectInfo.indirect_active_rate.mul(amount).div(1000);
+            unwithdrawList[inDirectMaster] = unwithdrawList[inDirectMaster].add(indirectValue);
+
+            totalUnWithdraw = totalUnWithdraw.add(indirectValue);
+        }
+
+    }
+
+
+
+    //Activate NFT rewards
+    function activateNFT(uint256 tokenId) public {
+        if (msg.sender != address(_NFT)) return;
+        address tokenOwner = _NFT.ownerOf(tokenId);
+        if (tokenOwner == address(0)) return;
+
+        SweepNFT.NFTData memory nft = _NFT.getTokenData(tokenId);
+        if (nft.tokenId != tokenId) return;
+
+        if (nftActiveList[tokenId] > 0) return;
+
+
+        if (masterRelationship[tokenOwner] == address(0)) return;
+
+        nftActiveList[tokenId] = block.timestamp;
+
+        uint256 nft_coin = nft.coin;
+        // Direct reward Master reward
+        address directMaster = masterRelationship[tokenOwner];
+
+
+        if (directMaster != address(0)) {
+            LevelInfo memory directInfo = getAddressLv(directMaster);
+
+            uint256 directValue = directInfo.direct_active_rate.mul(nft_coin).div(1000);
+            unwithdrawList[directMaster] = unwithdrawList[directMaster].add(directValue);
+
+            totalUnWithdraw = totalUnWithdraw.add(directValue);
+        }
+
+        //inDirect reward Master reward
+        if (masterRelationship[directMaster] != address(0)) {
+            address inDirectMaster = masterRelationship[directMaster];
+            LevelInfo memory indirectInfo = getAddressLv(inDirectMaster);
+
+            uint256 indirectValue = indirectInfo.indirect_active_rate.mul(nft_coin).div(1000);
+            unwithdrawList[inDirectMaster] = unwithdrawList[inDirectMaster].add(indirectValue);
+
+            totalUnWithdraw = totalUnWithdraw.add(indirectValue);
+        }
+
+    }
+
+
+    function withdrawCoin(address target) public view returns (uint256){
+        uint256 value = withdrawList[target];
+        return value;
+    }
+
+
+    function unWithdrawCoin(address target) public view returns (uint256){
+        uint256 value = unwithdrawList[target];
+        return value;
+    }
+
+
+    function withdraw() public {
+        require(address(msg.sender) == address(tx.origin), "no contract");
+        uint256 value = unwithdrawList[msg.sender];
+        require(value > 0, 'Withdrawal amount must be greater than 0');
+
+        _Token.transfer(msg.sender, value);
+        withdrawList[msg.sender] = withdrawList[msg.sender].add(value);
+        unwithdrawList[msg.sender] = 0;
+        totalWithdraw = totalWithdraw.add(value);
+        totalUnWithdraw = totalUnWithdraw.sub(value);
+    }
+
+}
